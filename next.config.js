f0f2461a632a976c981d16b82224c0769d8cb4fc
maelis123 <maelis.hammouche@hotmail.fr>
2024-06-27 09:39:27 +0200

    chore: update packages and next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
